81decccb02b132fe031222c1967a14294bdf98fa
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-29 14:39:13 +0300

    update: add transaction routes on app

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from routes.user_routes import router as user_routes
+from routes.transaction_routes import router as transaction_routes
 from core.database import init_db
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     init_db()
+@app.get("/")
+def welcome():
+    return { "message": "Welcome to Budget Management API !"}
 
 app.include_router(router=user_routes, prefix="/user", tags=["User Routes"])
+app.include_router(router=transaction_routes, prefix="/transaction", tags=["Transaction Routes"])
