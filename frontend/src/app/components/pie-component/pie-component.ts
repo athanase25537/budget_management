b77c03b5ed8b0991ff0205342bb3452341f3a780
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-31 21:56:42 +0300

    update: dashboard

--- frontend/src/app/components/pie-component/pie-component.ts
+++ frontend/src/app/components/pie-component/pie-component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartConfiguration } from 'chart.js';
-import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart, DoughnutController, ArcElement, Tooltip, Legend } from 'chart.js';
 
-// Enregistrement des éléments nécessaires pour le Pie Chart
-Chart.register(PieController, ArcElement, Tooltip, Legend);
+// Enregistrement des éléments nécessaires pour le Doughnut Chart
+Chart.register(DoughnutController, ArcElement, Tooltip, Legend);
 
 @Component({
   selector: 'app-pie-component',
@@ -15,31 +15,28 @@ Chart.register(PieController, ArcElement, Tooltip, Legend);
 })
 export class PieComponent {
   // Configuration des données
-  chartData: ChartConfiguration<'pie'>['data'] = {
-    labels: ['Alimentation', 'Logement', 'Transport', 'Loisirs', 'Épargne'],
+  chartData: ChartConfiguration<'doughnut'>['data'] = {
+    labels: ['Alimentation', 'Logement', 'Transport'],
     datasets: [{
-      data: [300, 500, 200, 150, 350],
+      data: [300, 500, 200],
       backgroundColor: [
         'rgba(255, 99, 132, 0.7)',
         'rgba(54, 162, 235, 0.7)',
-        'rgba(255, 206, 86, 0.7)',
-        'rgba(75, 192, 192, 0.7)',
-        'rgba(153, 102, 255, 0.7)'
+        'rgba(255, 206, 86, 0.7)'
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)'
+        'rgba(255, 206, 86, 1)'
       ],
       borderWidth: 1
     }]
   };
 
   // Options du graphique
-  chartOptions: ChartConfiguration<'pie'>['options'] = {
+  chartOptions: ChartConfiguration<'doughnut'>['options'] = {
     responsive: true,
+    cutout: '70%', // taille du trou (peut être ajustée, ex : '70%' pour plus grand trou)
     plugins: {
       legend: {
         position: 'right',
