d7e9eda6cefd83b2e36176f7d93447c787b9b4e7
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-08-03 21:29:32 +0300

    feat: stats component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from "./components/dashboard-component/dashboard-component";
+import { StatsComponent } from "./components/stats-component/stats-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet, DashboardComponent, StatsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- frontend/src/app/components/stats-component/stats-component.ts
+++ frontend/src/app/components/stats-component/stats-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GraphComponent } from "../graph-component/graph-component";
 
 @Component({
   selector: 'app-stats-component',
-  imports: [],
+  imports: [GraphComponent],
   templateUrl: './stats-component.html',
   styleUrl: './stats-component.scss'
 })
