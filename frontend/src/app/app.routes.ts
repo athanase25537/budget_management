19fc2e719b917ea7404983305301ff94ca34b48c
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-08-04 09:33:09 +0300

    feat: add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard-component/dashboard-component';
+import { StatsComponent } from './components/stats-component/stats-component';
+import { TransactionComponent } from './components/transaction-component/transaction-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: DashboardComponent },
+    { path: "dashboard", component: DashboardComponent },
+    { path: "stats", component: StatsComponent },
+    { path: "transactions", component: TransactionComponent },
+];
