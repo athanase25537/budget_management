f81fb9ff1f877393bd1884710776d0befbdfa141
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-08-05 16:22:29 +0300

    feat: make redirection for / to dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard-component/dashboard-component';
 import { StatsComponent } from './components/stats-component/stats-component';
 import { TransactionComponent } from './components/transaction-component/transaction-component';
-
+import { RedirectCommand } from '@angular/router';
 export const routes: Routes = [
-    { path: "", component: DashboardComponent },
+    { path: "", redirectTo: 'dashboard', pathMatch: 'full'},
     { path: "dashboard", component: DashboardComponent },
     { path: "stats", component: StatsComponent },
     { path: "transactions", component: TransactionComponent },
