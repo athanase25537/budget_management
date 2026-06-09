d57290711fb728cb97cd30a6e0974a4342df96fa
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-06-09 10:48:50 +0300

    fix: all table alembic

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,8 @@ from alembic import context
 from models.budget_management_models import *
 from sqlmodel import SQLModel
 from core.database import DATABASE_URL
+import sqlmodel
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- models/budget_management_models.py
+++ models/budget_management_models.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime
 from typing import List, Optional
+
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
@@ -14,36 +17,45 @@ class User(SQLModel, table=True):
         back_populates="user",
         sa_relationship_kwargs={"cascade": "all, delete-orphan"}
     )
-    
+
     setting: Optional["Setting"] = Relationship(
         back_populates="user",
         sa_relationship_kwargs={"cascade": "all, delete-orphan"}
     )
-    
-    
+
+
 class Category(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(unique=True, nullable=False)
-    transactions: List["Transaction"] = Relationship(back_populates="category")
+
+    transactions: List["Transaction"] = Relationship(
+        back_populates="category"
+    )
+
 
 class Transaction(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     amount: float = Field(nullable=False)
     is_in: bool = Field(default=True)
+
     user_id: int = Field(foreign_key="user.id")
+    category_id: Optional[int] = Field(default=None, foreign_key="category.id")
+
     date: Optional[datetime] = Field(default=None)
     reason: Optional[str] = Field(default=None)
-    category_id: Optional[int] = Field(default=None, foreign_key="category.id")
-    
-    user: Optional[User] = Relationship(back_populates="transactions")
+
+    user: Optional["User"] = Relationship(back_populates="transactions")
     category: Optional["Category"] = Relationship(back_populates="transactions")
+
 
 class Setting(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     economy: int = Field(nullable=False)
-    min_val_stat: int = Field(default=True)
-    max_val_stat: int = Field(default=None)
-    increment: int = Field(default=None)
-    
+
+    min_val_stat: int = Field(default=0)
+    max_val_stat: Optional[int] = Field(default=None)
+    increment: Optional[int] = Field(default=None)
+
     user_id: int = Field(foreign_key="user.id", unique=True)
-    user: Optional[User] = Relationship(back_populates="setting")
+
+    user: Optional["User"] = Relationship(back_populates="setting")
