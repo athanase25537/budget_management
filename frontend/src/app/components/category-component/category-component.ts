fe32239131ef16dc7324960d7b76b928ce514243
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-06-17 16:52:25 +0300

    update

--- frontend/src/app/components/category-component/category-component.ts
+++ frontend/src/app/components/category-component/category-component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MiniCard } from '../mini-card/mini-card';
+import { CategoryModel } from '../../models/category-model';
+import { BudgetService } from '../../services/budget-service';
+import { AuthService } from '../../services/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category-component',
-  imports: [],
+  imports: [MiniCard, CommonModule],
   templateUrl: './category-component.html',
   styleUrl: './category-component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: CategoryModel[] = [];
+  filteredCategories: CategoryModel[] = [];
 
+  constructor(private budgetService: BudgetService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    const user_id: number = this.authService.getCurrentUser()?.id || 1;
+    console.log(user_id)
+    this.budgetService.getCategoriesByUserId(user_id).subscribe({
+      next: (data: any) => {
+        this.categories = data;
+        this.filteredCategories = data;
+        console.log('Categories fetched successfully:', this.categories);
+      }
+    });
+  }
 }
