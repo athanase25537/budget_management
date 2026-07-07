391bf1f5b0f834b04a5c3d84c967c1a7026e6876
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-07-07 10:08:24 +0300

    update (app.config): add interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,14 +2,19 @@ import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provi
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor
+      ])
+    ),
     {provide: LOCALE_ID, useValue: 'fr-FR' }
   ]
 };
