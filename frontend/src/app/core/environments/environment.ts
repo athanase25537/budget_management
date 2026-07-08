0b418267179546050d776d9d72c2827bfc870306
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-07-08 17:12:20 +0300

    refactor

--- frontend/src/app/core/environments/environment.ts
+++ frontend/src/app/core/environments/environment.ts
@@ -1,7 +1,7 @@
 // environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000',
+    apiUrl: 'http://172.16.87.25:8000',
     items_per_page: 5,
     category_per_page: 10
   };
