1f7d880be77b43bd1add7c8c7cd2467b54fc0b36
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-07-08 14:21:47 +0300

    feat (category-store): delete/update/create category

--- frontend/src/app/core/data/category-store.ts
+++ frontend/src/app/core/data/category-store.ts
@@ -8,6 +8,7 @@ import { SettingsModel } from "../models/settings-model";
 import { CategoryModel } from "../models/category-model";
 import { TableTransactionModel } from "../models/table-transaction-model";
 import { AuthService } from "../services/auth-service";
+import { TableCategoryModel } from "../models/table-category-model";
 
 @Injectable({
     providedIn: 'root'
@@ -15,16 +16,16 @@ import { AuthService } from "../services/auth-service";
 
 export class CategoryStore {
 
-    private cacheCategories = new Map<number, CategoryModel[]>();
+    private cacheCategories = new Map<number, TableCategoryModel>();
 
-    private categoriesSubject = new BehaviorSubject<CategoryModel[] | undefined>(undefined);
+    private categoriesSubject = new BehaviorSubject<TableCategoryModel | undefined>(undefined);
     private hasNextPageSubject = new BehaviorSubject<number>(0);
     private hasPreviousPageSubject = new BehaviorSubject<number>(0);
     private totalCategorySubject = new BehaviorSubject<number>(0);
     private totalPageSubject = new BehaviorSubject<number>(0);
     private elementPerPageSubject = new BehaviorSubject<number>(0);
 
-    categories$: Observable<CategoryModel[] | undefined> = this.categoriesSubject.asObservable();
+    categories$: Observable<TableCategoryModel | undefined> = this.categoriesSubject.asObservable();
     hasNextPage: Observable<number> = this.hasNextPageSubject.asObservable();
     hasPreviousPage: Observable<number> = this.hasPreviousPageSubject.asObservable();
     totalCategory: Observable<number> = this.totalCategorySubject.asObservable();
@@ -43,15 +44,21 @@ export class CategoryStore {
             const user_id: number = this.authService.getCurrentUser()?.id || 1;
             this.budgetService.getCategoriesByUserId(user_id, this.page).subscribe({
                 next: (data: any) => {
-                    this.categoriesSubject.next(data.categories)
-                    this.hasNextPageSubject.next(data.has_next_page);
-                    this.hasPreviousPageSubject.next(data.has_previous_page);
-                    this.totalCategorySubject.next(data.total);
+
+                    let table_data = new TableCategoryModel(
+                        data.categories,
+                        data.has_next_page,
+                        data.has_previous_page,
+                        data.current_page,
+                        data.element_per_page,
+                        data.total
+                    );
+
+                    this.cacheCategories.set(this.page, table_data)
                     
-                    let totalPage = Math.ceil(this.totalCategorySubject.value / data.element_per_page);
+                    let totalPage = Math.ceil(data.total / data.element_per_page);
                     this.totalPageSubject.next(totalPage);
 
-                    this.elementPerPageSubject.next(data.element_per_page);
                 }
             });
         }
@@ -59,7 +66,20 @@ export class CategoryStore {
     }
 
     onCreate() {
+
         
+        this.resetCache();
+        this.resetCategory();
+    }
+
+    onUpdate() {
+        this.resetCache();
+        this.resetCategory();
+    }
+
+    onDelete() {
+        this.resetCache();
+        this.resetCategory();
     }
 
     private resetCache() {
