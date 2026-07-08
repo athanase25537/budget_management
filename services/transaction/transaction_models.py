d6da97573ba0f64812dc99a21c8c4ad33b4e0119
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-07-08 15:48:09 +0300

    update (transaction_model): make reason optional

--- services/transaction/transaction_models.py
+++ services/transaction/transaction_models.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 class Transaction_create(BaseModel):
     amount: float
     is_in: bool
     user_id: int
     date: datetime
-    reason: str
+    reason: Optional[str] = None
     category_id: int
 
 class Transaction_update(BaseModel):
     amount: float
     is_in: bool
     date: datetime
-    reason: str
+    reason: Optional[str] = None
     category_id: int
