ed2ed8b6aa5ab3137a4fa5ee1d4b6bba28028c4a
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-07-19 12:48:40 +0300

    update (category_models/category_services): add categoryType

--- backend/services/category/category_models.py
+++ backend/services/category/category_models.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
 from typing import Optional
+from backend.models.budget_management_models import CategoryType
 
 class Category_create(BaseModel):
     name: str 
     user_id: Optional[int] = None
     color: str
+    type: CategoryType = CategoryType.INCOME
 
 class Category_update(BaseModel):
     name: str
     color: str
+    type: CategoryType = CategoryType.INCOME
